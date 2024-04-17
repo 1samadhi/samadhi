a751dd60206e31a0b6e61eceb4404a93dabbf698
ismael <ismaeloyarzun95@gmail.com>
2024-04-17 18:01:30 -0400

    hola

---
+++ CtoF.py
@@ -0,0 +1,3 @@
+c = int(input ("ingrese los grados celcius ")) #declaramos c como un entero con la variable int, luego input para ingresar datos y que queden almacenados como "c"
+f = c - 32 * 5/9 #indicamos que para "f" aplique la fórmula 
+print(c, "grados celcius equivalen a ", f) #imprimimos que c grados equivalen a f que aplicará la fórmula antes detallada
