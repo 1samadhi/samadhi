c2068d60190a2046cf92469e2741b4730e7ad690
ismael <ismaeloyarzun95@gmail.com>
2024-04-17 18:30:37 -0400

    hola

---
+++ FtoC.py
@@ -0,0 +1,3 @@
+f = int(input ("ingrese los grados fahrenheit ")) #declaramos f como un entero con la variable int, luego input para ingresar datos y que queden almacenados como "f"
+c = (f - 32) * 5/9 #indicamos que para "c" aplique la fórmula 
+print(f, "grados fahrenheit equivalen a ", c, "grados celcius") #imprimimos que f grados equivalen a c que aplicará la fórmula antes detallada
